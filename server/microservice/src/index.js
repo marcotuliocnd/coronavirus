04fb1c0f21586c35ccbe4004a144f5c9c6939ba6
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-02-27 21:01:08 -0300

    re-factored code

--- server/microservice/src/index.js
+++ server/microservice/src/index.js
@@ -1,10 +1,21 @@
-const wikipediaService = require('./services/wikipedia');
-const APIService = require('./services/sendToAPI');
 const cron = require('node-cron');
+require('dotenv').config();
+
+const Wikipedia = require('./services/Wikipedia');
+const Api = require('./services/Api');
 
 cron.schedule('*/5 * * * *', async () => {
   console.log('Started to fetch data');
-  const allCountriesData = await wikipediaService.getParsedData();
-  APIService.postCountriesDataToAPI(allCountriesData);
+  const allCountriesData = await Wikipedia.getParsedData();
+  Api.postCountriesDataToAPI(allCountriesData);
   console.log('Finished fetching data');
 });
+
+const teste = async () => {
+  console.log('Started to fetch data');
+  const allCountriesData = await Wikipedia.getParsedData();
+  Api.postCountriesDataToAPI(allCountriesData);
+  console.log('Finished fetching data');
+};
+
+teste();
