eb94ef1848bd440bdd791ab1a24b6d3427e77845
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-03-03 23:42:56 -0300

    now posting to api /totals

--- server/microservice/src/services/Api.js
+++ server/microservice/src/services/Api.js
@@ -9,16 +9,35 @@ const api = axios.create({
 
 const postCountriesDataToAPI = async (countriesData) => {
   let index = 0;
+
+  const total = {
+    totalInfecteds:0,
+    totalSurvivors:0,
+    totalDeaths:0
+  };
+
   for (const country of countriesData) {
     index += 1;
     console.log(`POSTING ${index}/${countriesData.length} COUNTRIES`);
     try {
+      total.totalDeaths += country.totalDeaths;
+      total.totalInfecteds += country.totalInfecteds;
+      total.totalSurvivors += country.totalSurvivors;
+
       await api.post('/countries', country);
-      console.log('SUCCESS');
+      console.log('SUCCESS POSTING TO API /countries');
     } catch (err) {
-      console.log('ERROR');
+      console.log('ERROR WHEN POSTING TO API /countries')
       console.error(err.message);
     }
+  }
+
+  try {
+    await api.post('/totals',total)
+    console.log('SUCCESS POSTING TO API /totals');
+  } catch (err) {
+    console.log('ERROR WHEN POSTING TO API /total')
+    console.error(err.message)
   }
 };
 
