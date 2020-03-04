c5719b7395856b25c2399d206438ef3b1ee4c4c2
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-03-04 20:41:49 -0300

    added new rules to eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -24,5 +24,11 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-underscore-dangle": "off",
+    'no-shadow': 'off',
+    'react/forbid-prop-types': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-nested-ternary': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   },
 };
