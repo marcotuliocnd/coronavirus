7b9038f08e01f3ea6ea55c28b481447131b761f5
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-03-09 22:21:34 -0300

    added announce dinamic

--- client/src/components/Anuncio/index.js
+++ client/src/components/Anuncio/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-const Anuncio = () => (
+const Anuncio = ({ statusState }) => (
   <div className="ad">
     <div className="ad--Inner sombra">
       <img
-        src="https://workana.s3.amazonaws.com/projects/1202215/e6/8697b5b20a4e7eb31964d464c82a4b/Fakenews_corona.png?response-content-disposition=inline%3Bfilename%3D%22Fakenews_corona.png%22&response-content-type=image%2Fpng&AWSAccessKeyId=AKIAIMMAP2ONURYRGXEQ&Expires=1583555378&Signature=IpWKi2GuXCF7y4Ly955HEMNdIS0%3D"
+        src={statusState.data.announcementRectangle}
         alt="Anuncio"
       />
     </div>
   </div>
 );
 
-export default Anuncio;
+Anuncio.propTypes = {
+  statusState: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  statusState: state.statusReducer,
+});
+
+export default connect(mapStateToProps)(Anuncio);
--- client/src/components/AnuncioSquare/index.js
+++ client/src/components/AnuncioSquare/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-const AnuncioSquare = () => (
+const AnuncioSquare = ({ statusState }) => (
   <div className="ad-square">
     <div className="ad-square--Inner sombra">
-      <img src="https://workana.s3.amazonaws.com/projects/1202215/2b/02b2974dbf4bb0b074c77119a6c275/Banner_fakenews_quadrado.png?response-content-disposition=inline%3Bfilename%3D%22Banner_fakenews_quadrado.png%22&response-content-type=image%2Fpng&AWSAccessKeyId=AKIAIMMAP2ONURYRGXEQ&Expires=1583555378&Signature=YCTCVCfkc1xN1hzBuoqz1IpBfLY%3D" alt="Anuncio" />
+      <img src={statusState.data.announcementSquare} alt="Anuncio" />
     </div>
   </div>
 );
 
-export default AnuncioSquare;
+AnuncioSquare.propTypes = {
+  statusState: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  statusState: state.statusReducer,
+});
+
+export default connect(mapStateToProps)(AnuncioSquare);
