dbef5e99380f71eed9d51a8be3bd8c679196cf0b
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-03-10 20:04:53 -0300

    feature: added dinamic text to footer

--- client/src/components/Coronavirus/index.js
+++ client/src/components/Coronavirus/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-const Coronavirus = () => (
+const Coronavirus = ({ status }) => (
   <div className="Coronavirus sombra">
     <div className="Coronavirus--inner">
-      <h2>
-        <h2 className="box-title articles-title">Coronavírus</h2>
+        <h2 className="box-title articles-title">{ status.data.coronaTitle }</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor
-          incididunt ut labore et dolore
-          magna aliqua. Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris.
-          Consectetur purus ut faucibus pulvinar elementum.
-          Erat pellentesque adipiscing commodo elit at.
-          Consequat ac felis donec et odio.
-          {' '}
-          <br />
-          Placerat vestibulum lectus mauris ultrices. Odio ut enim blandit volutpat
-          maecenas volutpat blandit. Odio facilisis mauris
-          sit amet massa vitae tortor condimentum. Vitae proin sagittis nisl rhoncus
-          mattis rhoncus. Adipiscing vitae proin sagittis nisl rhoncus.ORONAVÍRUS
+          { status.data.coronaText }
         </p>
-      </h2>
     </div>
   </div>
 );
 
-export default Coronavirus;
+Coronavirus.propTypes = {
+  status: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  status: state.statusReducer,
+});
+
+export default connect(mapStateToProps)(Coronavirus);
