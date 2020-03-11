91cba624b2c8ca4ffe078adf633b2f0f7a55506a
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-03-11 00:05:23 -0300

    added deleting message while send req

--- client/src/pages/Articles/index.js
+++ client/src/pages/Articles/index.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import ReactHtmlParser from 'react-html-parser';
 import Helmet from 'react-helmet';
 import { show } from '../../actions/Article';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 import Loading from '../../components/Loading';
 
@@ -17,6 +19,11 @@ const Articles = ({ match, show, article, loading }) => {
   useEffect(() => {
     show(match.params.article);
   }, []);
+
+  function formatDate(datetime) {
+    moment.locale('pt-br');
+    return moment(datetime).calendar();
+  }
 
   return loading ? <Loading /> : !article ? <NotFound /> : (
     <>
@@ -33,6 +40,7 @@ const Articles = ({ match, show, article, loading }) => {
           <div className="articles-header">
             <h1>{ article.title }</h1>
             <h2>{ article.description }</h2>
+            <h3 className="datetime">{ formatDate(article.createdAt) }</h3>
             <hr />
           </div>
           <div className="articles-content">
