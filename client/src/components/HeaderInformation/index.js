5f55c4884bedba7a635f28a4f04d7345c1dbc028
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-03-11 22:19:08 -0300

    fixed text in legal warning

--- client/src/components/HeaderInformation/index.js
+++ client/src/components/HeaderInformation/index.js
@@ -8,18 +8,18 @@ import Logo from '../../assets/images/logo.png';
 const HeaderInformation = () => (
   <div className="header">
     <Navbar bg="transparent" expand="lg">
-    <Navbar.Brand href="/"><img className="logo" src={Logo} alt="logomarca" /></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand href="/"><img className="logo" src={Logo} alt="logomarca" /></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-        <div className="header--Textbox">
-          <h1>Coronavírus - COVID-19 - ESTATÍSTICAS GLOBAIS (Último surto)</h1>
-          <p>
-            Aviso legal. Os dados apresentados nesse site são meramente informativos,
-            e não devem ser utilizados para nenhuma orientação médica ou comércio.
-          </p>
-          <Link to="/termos">Leia os termos de uso.</Link>
-        </div>
+          <div className="header--Textbox">
+            <h1>Coronavírus - COVID-19 - ESTATÍSTICAS GLOBAIS (Último surto)</h1>
+            <p>
+              Aviso legal. Os dados apresentados neste website são meramente informativos,
+              e não devem ser utilizados para nenhuma orientação médica ou comércio.
+            </p>
+            <Link to="/termos">Leia os termos de uso.</Link>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
