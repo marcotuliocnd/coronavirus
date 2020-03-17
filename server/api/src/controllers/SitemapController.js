8e6179f8eb6693f6aa488c1f58feca4ffa1d7f5b
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-03-17 01:20:42 -0300

    arrumei a burrice do ken

--- server/api/src/controllers/SitemapController.js
+++ server/api/src/controllers/SitemapController.js
@@ -2,8 +2,8 @@ const ArticleModel = require('../models/Article');
 
 const sitemap = async () => {
   try {
-    const { link } = await ArticleModel.find().lean();
-    return link.map((item) => `https://coronavirus.com.br/${item}`);
+    const data = await ArticleModel.find().lean();
+    return data.map((item) => `https://coronavirus.com.br/${item.link}`);
   } catch (err) {
     console.error(err.message);
     return [];
